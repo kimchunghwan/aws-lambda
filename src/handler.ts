498bd3803dd2bc30ee2bca50042bd2009eecbd7d
chunghwan kim <keii2k84@gmail.com>
2024-11-17 12:08:28 +0900

    edit

---
+++ src/handler.ts
@@ -0,0 +1,17 @@
+import { APIGatewayEvent, Context, Callback, APIGatewayProxyResult } from 'aws-lambda';
+
+export const handler = async (
+  event: APIGatewayEvent,
+  context: Context,
+  callback: Callback
+): Promise<APIGatewayProxyResult> => {
+  console.log('Received event:', JSON.stringify(event, null, 2));
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Hello from Lambda! updated 11111',
+      input: event,
+    }),
+  };
+};
---
+++ terraform/lambda.tf
@@ -0,0 +1,26 @@
+resource "aws_lambda_function" "example_lambda" {
+    function_name = "example-lambda"
+    handler       = "dist/handler.handler"
+    runtime       = "nodejs20.x"
+    publish       = true
+
+    role          = aws_iam_role.lambda_role.arn
+    filename      = "../function.zip" # zip 파일 경로
+
+    source_code_hash = filebase64sha256("../function.zip")
+}
+
+resource "aws_lambda_alias" "example_lambda_alias" {
+    name             = "live"
+    function_name    = aws_lambda_function.example_lambda.function_name
+    function_version = aws_lambda_function.example_lambda.version
+}
+
+# resource "aws_lambda_provisioned_concurrency_config" "example_lambda_concurrency" {
+#     function_name                     = aws_lambda_function.example_lambda.function_name
+#     qualifier                         = aws_lambda_alias.example_lambda_alias.name
+#     provisioned_concurrent_executions = 0
+# }
+
+
+
