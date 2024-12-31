483f8a29ed4ad74f9314e5741b1767914039b847
chunghwan kim <keii2k84@gmail.com>
2024-12-31 10:27:33 +0900

    Add new Lambda function and update IAM policies for invocation

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -10,13 +10,35 @@ resource "aws_iam_role" "lambda_role" {
         Principal = {
           Service = "lambda.amazonaws.com"
         }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_policy" "lambda_invoke_policy" {
+  name = "lambda-invoke-policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action   = "lambda:InvokeFunction",
+        Effect   = "Allow",
+        Resource = "arn:aws:lambda:ap-northeast-1:044264552816:function:function_01"
       }
     ]
   })
 }
 
+
 resource "aws_iam_policy_attachment" "lambda_execution_policy" {
   name       = "lambda-execution-policy"
   roles      = [aws_iam_role.lambda_role.name]
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_iam_policy_attachment" "lambda_invoke_policy_attachment" {
+  name       = "lambda-invoke-policy-attachment"
+  roles      = [aws_iam_role.lambda_role.name]
+  policy_arn = aws_iam_policy.lambda_invoke_policy.arn
 }
--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,19 +1,31 @@
 resource "aws_lambda_function" "api_v1" {
-    function_name = "api_v1"
-    handler       = "dist/handler.handler"
-    runtime       = "nodejs20.x"
-    publish       = true
+  function_name = "api_v1"
+  handler       = "dist/handler.handler"
+  runtime       = "nodejs20.x"
+  publish       = true
 
-    role          = aws_iam_role.lambda_role.arn
-    filename      = "../function.zip" # zip 파일 경로
+  role     = aws_iam_role.lambda_role.arn
+  filename = "../function.zip" # zip 파일 경로
 
-    source_code_hash = filebase64sha256("../function.zip")
+  source_code_hash = filebase64sha256("../function.zip")
+}
+
+resource "aws_lambda_function" "function_01" {
+  function_name = "function_01"
+  handler       = "dist/function_01.handler"
+  runtime       = "nodejs20.x"
+  publish       = true
+
+  role     = aws_iam_role.lambda_role.arn
+  filename = "../function.zip" # zip 파일 경로
+
+  source_code_hash = filebase64sha256("../function.zip")
 }
 
 resource "aws_lambda_alias" "example_lambda_alias" {
-    name             = "live"
-    function_name    = aws_lambda_function.api_v1.function_name
-    function_version = aws_lambda_function.api_v1.version
+  name             = "live"
+  function_name    = aws_lambda_function.api_v1.function_name
+  function_version = aws_lambda_function.api_v1.version
 }
 
 
